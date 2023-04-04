a2bfe1618154adb8a10d7e92bc96a8dfe20fcbdc
VictoriaKhegay <victoria.khegay@gmail.com>
2023-04-04 18:09:30 +0600

    add site

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('button');
+const form = document.querySelector('#popup');
+const popup = document.querySelector('.popup');
+
+button.addEventListener ('click'  () => {
+    if{
+  form.classList.add('open');
+  popup.classList.add('popup_open');
+} else{
+    console.log(error);
+}
+
+});
